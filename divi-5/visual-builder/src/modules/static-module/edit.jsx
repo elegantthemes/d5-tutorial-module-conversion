ebf0540325e9240ba742e77f98216b14de770224
Raju Ahmmed <rajuahmmedbd@gmail.com>
2023-03-06 23:12:32 +0600

    Rebuilding module render output in Visual Builder for Static module

--- divi-5/visual-builder/src/modules/static-module/edit.jsx
+++ divi-5/visual-builder/src/modules/static-module/edit.jsx
@@ -1,3 +1,27 @@
-export const StaticModuleEdit = () => (
-    <div className="static-module">boilerplate component</div>
-)
+const { ModuleContainer } = divi.module;
+
+export const StaticModuleEdit = ({
+  attrs,
+  id,
+  name,
+}) => {
+  const HeaderLevel = attrs?.titleFont?.font?.desktop?.value?.headingLevel ?? 'h2';
+  const title       = attrs?.title?.desktop?.value ?? '';
+  const content     = attrs?.content?.desktop?.value ?? '';
+
+  return (
+    <ModuleContainer
+      attrs={attrs}
+      id={id}
+      name={name}
+    >
+      <div className="et_pb_module_inner">
+        <HeaderLevel className="dtmc_static_module_title">{title}</HeaderLevel>
+        <div
+          className="dtmc_static_module_content"
+          dangerouslySetInnerHTML={{__html: content}}
+        />
+      </div>
+    </ModuleContainer>
+  );
+}
